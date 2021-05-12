141175d9819e4a1defb3675cb566edb6e0d332fd
David Siemionko <dsiemionko4406@stu.d214.org>
2021-05-12 14:32:16 -0500

    Initial Commit

---
+++ MapKit and CoreLocation Lecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKit and CoreLocation Lecture
+//
+//  Created by David Siemionko on 5/12/21.
+//  Copyright © 2021 David Siemionko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
